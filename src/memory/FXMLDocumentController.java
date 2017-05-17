4f04c8ce88f2c6e057395c448af5a4cbaaf63b43
HydrusWP <jakobsb.mail@gmail.com>
2017-05-17 21:19:06 +0200

    Test 3

--- src/memory/FXMLDocumentController.java
+++ src/memory/FXMLDocumentController.java
@@ -1,3 +1,4 @@
+package memory;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
--- src/memory/JFXApplication.java
+++ src/memory/JFXApplication.java
@@ -1,9 +1,12 @@
+package memory;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+
+import java.io.File;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -15,23 +18,36 @@ import javafx.stage.Stage;
  *
  * @author D067928
  */
-public class JavaFXApplication2 extends Application {
+public class JFXApplication extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
+    	
+        String filePathString = "H:\\EclipseGit\\Memory\\memory\\src\\memory\\FXMLDoc.fxml";
+        System.out.println("Test");
+        File f = new File(filePathString);
+        if(f.exists()) { 
+            System.out.println("File exists");
+            System.out.println(f.getAbsoluteFile());
+            
+            
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("FXMLDoc.fxml"));
+            Parent root = loader.load();
+            
+            Scene scene = new Scene(root);
+            
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+    	launch(args);
+        
     }
     
 }
